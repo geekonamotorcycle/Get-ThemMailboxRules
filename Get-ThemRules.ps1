f948d1a6a5c7b15250c254f04860ec60f449ec03
A certain person <Joshua.porrata@gmail.com>
2022-12-02 00:46:36 -0500

    Update Get-ThemRules.ps1
    * Rewritten to display progress information
    * Fixed a loop bug where not all rules were being recorded
    * Added Forwarding report

--- Get-ThemRules.ps1
+++ Get-ThemRules.ps1
@@ -1,15 +1,21 @@
 <#
 .SYNOPSIS
     This script will return a CSV with all of the rules found for all mailboxes in exchange online
+    It will also output the results of a search for all mailboxes with forwarding enabled
 .DESCRIPTION
-    Initializes an empty $Array
-    Retrieves a list of mailbox UserPrincipalName
-    Iterates through the list of UserPrincipalName with the Get-InboxRule cmdlet
-    Loads reults into a PSCustomObject and adds to $Array
-    Outputs to a CSV in the same folder the Script was run from (will over write)
+    1.  checks for Exchangeonline session and starts a session if none is detected
+    2.  intializes an empty $Array
+    3.  Retrieves a list of mailbox UserPrincipalName
+    4.  Iterates through the list of UserPrincipalName with the Get-InboxRule cmdlet
+    5.  Loads reults into a PSCustomObject and adds to $Array
+    6.  Outputs to a CSV in the same folder the Script was run from (will overwrite)
+    Also
+    1.  searches for SMTP forwarding
+    2.  Outputs to a CSV in the same folder the Script was run from (will overwrite)
+
 .NOTES
-    Last reveiwed: 2022-12-010
-    Version: 1
+    Last reveiwed: 2022-12-02
+    Version: 1.5
     Written By: Joshua Porrata
     Email: joshua.porrata@gmail.com
     License: Not free for commercial use without permission
@@ -35,18 +41,18 @@ if (!(
 };
 
 # Get Mailbox UPN list
-$mailboxes = get-mailbox | Select-Object UserPrincipalName;
-$Results = foreach ($box in $mailboxes) { Get-InboxRule -mailbox $box.UserPrincipalName.ToString() };
-Write-Host "there are " $Results.count "Rules Found"
+$MailboxList = get-mailbox | Select-Object UserPrincipalName;
+Write-Host "there are " $MailboxList.count "mailbox to check" -ForegroundColor Cyan;
+
 # Develop file and path names based on date and time
-$Date = Get-Date;
+$Date = (Get-Date);
 $DateStr = $Date.ToString("yyyyMMdd-HHmm");
 $filename = $DateStr + "_MailboxRulesReport.csv";
 $BasePath = Get-Location;
-$fullpath = Join-Path -Path $BasePath -ChildPath $filename;
+$FullPath = Join-Path -Path $BasePath -ChildPath $filename;
 
 # set to True to spam your screen with Results (best for debugging)
-$verbose = $False;
+$Verbose = $False;
 
 # Initialize $Array
 $Array = @();
@@ -55,78 +61,92 @@ $Array = @();
 $ErrorActionPreference = "silentlycontinue";
 
 # begin rule lookup loop
-foreach ($MailBox in $Results) {
+for ($i = 0; $i -lt $MailboxList.Count; $i++) {
     
-    if ($verbose) {
-        Write-Host "Identity: " $MailBox.Identity
-        Write-Host "IsValid: "$MailBox.IsValid
-        Write-Host "Enabled: "$MailBox.Enabled
-        Write-Host "Legacy: "$MailBox.Legacy
-        Write-Host "RuleIdentity: "$MailBox.RuleIdentity
-        Write-Host "MailboxOwnerId: "$MailBox.MailboxOwnerId
-        Write-Host "Name: "$MailBox.Name
-        Write-Host "From: "$MailBox.From
-        Write-Host "SentTo: "$MailBox.SentTo
-        Write-Host "HeaderContainsWords: "$MailBox.HeaderContainsWords
-        Write-Host "SubjectContainsWords: "$MailBox.SubjectContainsWords
-        Write-Host "BodyContainsWords: "$MailBox.BodyContainsWords
-        Write-Host "SoftDeleteMessage: "$MailBox.SoftDeleteMessage
-        Write-Host "DeleteMessage: "$MailBox.DeleteMessage
-        Write-Host "ForwardTo: "$MailBox.ForwardTo
-        Write-Host "ForwardAsAttachmentTo: "$MailBox.ForwardAsAttachmentTo
-        Write-Host "MoveToFolder: "$MailBox.MoveToFolder
-        Write-Host "RedirectTo: "$MailBox.RedirectTo
-        Write-Host "Description: "$MailBox.Description
-    };
-    
-    #some logic that doesnt really work
-
-    if ($null -ne $MailBox.Identity) {}else { $MailBox.Identity = " " };
-    if ($null -ne $MailBox.IsValid) {}else { $MailBox.IsValid = " " };
-    if ($null -ne $MailBox.Enabled) {}else { $MailBox.Enabled = " " };
-    if ($null -ne $MailBox.Legacy) {}else { $MailBox.Legacy = " " };
-    if ($null -ne $MailBox.RuleIdentity) {}else { $MailBox.RuleIdentity = " " };
-    if ($null -ne $MailBox.MailboxOwnerId) {}else { $MailBox.MailboxOwnerId = " " };
-    if ($null -ne $MailBox.From) {}else { $MailBox.From = " " };
-    if ($null -ne $MailBox.SentTo) {}else { $MailBox.SentTo = " " };
-    if ($null -ne $MailBox.HeaderContainsWords) {}else { $MailBox.HeaderContainsWords = " " };
-    if ($null -ne $MailBox.SubjectContainsWords) {}else { $MailBox.SubjectContainsWords = " " };
-    if ($null -ne $MailBox.BodyContainsWords) {}else { $MailBox.BodyContainsWords = " " };
-    if ($null -ne $MailBox.SoftDeleteMessage) {}else { $MailBox.SoftDeleteMessage = " " };
-    if ($null -ne $MailBox.DeleteMessage) {}else { $MailBox.DeleteMessage = " " };
-    if ($null -ne $MailBox.ForwardTo) {}else { $MailBox.ForwardTo = " " };
-    if ($null -ne $MailBox.ForwardAsAttachmentTo) {}else { $MailBox.ForwardAsAttachmentTo = " " };
-    if ($null -ne $MailBox.MoveToFolder) {}else { $MailBox.MoveToFolder = " " };
-    if ($null -ne $MailBox.RedirectTo) {}else { $MailBox.RedirectTo = " " };
-    if ($null -ne $MailBox.Description) {}else { $MailBox.Description = " " };
-
-    $Array += [PSCustomObject]@{
-        Identity              = $MailBox.Identity
-        IsValid               = $MailBox.IsValid
-        Enabled               = $MailBox.Enabled
-        Legacy                = $MailBox.Legacy
-        RuleIdentity          = $MailBox.RuleIdentity
-        MailboxOwnerId        = $MailBox.MailboxOwnerId
-        Name                  = $MailBox.Name
-        From                  = $MailBox.From
-        SentTo                = $MailBox.SentTo
-        HeaderContainsWords   = $MailBox.HeaderContainsWords
-        SubjectContainsWords  = $MailBox.SubjectContainsWords
-        BodyContainsWords     = $MailBox.BodyContainsWords
-        SoftDeleteMessage     = $MailBox.SoftDeleteMessage
-        DeleteMessage         = $MailBox.DeleteMessage
-        RedirectTo            = $MailBox.RedirectTo
-        ForwardTo             = $MailBox.ForwardTo
-        ForwardAsAttachmentTo = $MailBox.ForwardAsAttachmentTo
-        MoveToFolder          = $MailBox.MoveToFolder
-        Description           = $MailBox.Description
+    $Row = Get-InboxRule -Mailbox $MailBoxlist[$i].UserPrincipalName.ToString() -IncludeHidden;
+    Write-Host "User: " $MailBoxlist[$i].UserPrincipalName.ToString() "Number: " $i "/" $MailboxList.Count -ForegroundColor Green;
+    for ($inner = 0; $inner -lt $Row.Count; $inner++) {
+        Write-Host `t $Row[$inner].Identity " Rule #: " $inner "/" $Row.Count -ForegroundColor Yellow;
+        if ($verbose) {
+            Write-Host "Identity: " $Row[$inner].Identity
+            Write-Host "IsValid: "$Row[$inner].IsValid
+            Write-Host "Enabled: "$Row[$inner].Enabled
+            Write-Host "Legacy: "$Row[$inner].Legacy
+            Write-Host "RuleIdentity: "$Row[$inner].RuleIdentity
+            Write-Host "MailboxOwnerId: "$Row[$inner].MailboxOwnerId
+            Write-Host "Name: "$Row[$inner].Name
+            Write-Host "From: "$Row[$inner].From
+            Write-Host "SentTo: "$Row[$inner].SentTo
+            Write-Host "HeaderContainsWords: "$Row[$inner].HeaderContainsWords
+            Write-Host "SubjectContainsWords: "$Row[$inner].SubjectContainsWords
+            Write-Host "BodyContainsWords: "$Row[$inner].BodyContainsWords
+            Write-Host "SoftDeleteMessage: "$Row[$inner].SoftDeleteMessage
+            Write-Host "DeleteMessage: "$Row[$inner].DeleteMessage
+            Write-Host "ForwardTo: "$Row[$inner].ForwardTo
+            Write-Host "ForwardAsAttachmentTo: "$Row[$inner].ForwardAsAttachmentTo
+            Write-Host "MoveToFolder: "$Row[$inner].MoveToFolder
+            Write-Host "RedirectTo: "$Row[$inner].RedirectTo
+            Write-Host "Description: "$Row[$inner].Description`n
+        };
+        # Enter Blank spaces for Null Values
+        
+        if ($null -ne $Row[$inner].Identity) {}else { $Row[$inner].Identity = " " };
+        if ($null -ne $Row[$inner].IsValid) {}else { $Row[$inner].IsValid = " " };
+        if ($null -ne $Row[$inner].Enabled) {}else { $Row[$inner].Enabled = " " };
+        if ($null -ne $Row[$inner].Legacy) {}else { $Row[$inner].Legacy = " " };
+        if ($null -ne $Row[$inner].RuleIdentity) {}else { $Row[$inner].RuleIdentity = " " };
+        if ($null -ne $Row[$inner].MailboxOwnerId) {}else { $Row[$inner].MailboxOwnerId = " " };
+        if ($null -ne $Row[$inner].From) {}else { $Row[$inner].From = " " };
+        if ($null -ne $Row[$inner].SentTo) {}else { $Row[$inner].SentTo = " " };
+        if ($null -ne $Row[$inner].HeaderContainsWords) {}else { $Row[$inner].HeaderContainsWords = " " };
+        if ($null -ne $Row[$inner].SubjectContainsWords) {}else { $Row[$inner].SubjectContainsWords = " " };
+        if ($null -ne $Row[$inner].BodyContainsWords) {}else { $Row[$inner].BodyContainsWords = " " };
+        if ($null -ne $Row[$inner].SoftDeleteMessage) {}else { $Row[$inner].SoftDeleteMessage = " " };
+        if ($null -ne $Row[$inner].DeleteMessage) {}else { $Row[$inner].DeleteMessage = " " };
+        if ($null -ne $Row[$inner].ForwardTo) {}else { $Row[$inner].ForwardTo = " " };
+        if ($null -ne $Row[$inner].ForwardAsAttachmentTo) {}else { $Row[$inner].ForwardAsAttachmentTo = " " };
+        if ($null -ne $Row[$inner].MoveToFolder) {}else { $Row[$inner].MoveToFolder = " " };
+        if ($null -ne $Row[$inner].RedirectTo) {}else { $Row[$inner].RedirectTo = " " };
+        if ($null -ne $Row[$inner].Description) {}else { $Row[$inner].Description = " " };
+        
+        $Array += [PSCustomObject]@{
+            Identity              = $Row[$inner].Identity
+            IsValid               = $Row[$inner].IsValid
+            Enabled               = $Row[$inner].Enabled
+            Legacy                = $Row[$inner].Legacy
+            RuleIdentity          = $Row[$inner].RuleIdentity
+            RowOwnerId            = $Row[$inner].MailboxOwnerId
+            Name                  = $Row[$inner].Name
+            From                  = $Row[$inner].From
+            SentTo                = $Row[$inner].SentTo
+            HeaderContainsWords   = $Row[$inner].HeaderContainsWords
+            SubjectContainsWords  = $Row[$inner].SubjectContainsWords
+            BodyContainsWords     = $Row[$inner].BodyContainsWords
+            SoftDeleteMessage     = $Row[$inner].SoftDeleteMessage
+            DeleteMessage         = $Row[$inner].DeleteMessage
+            RedirectTo            = $Row[$inner].RedirectTo
+            ForwardTo             = $Row[$inner].ForwardTo
+            ForwardAsAttachmentTo = $Row[$inner].ForwardAsAttachmentTo
+            MoveToFolder          = $Row[$inner].MoveToFolder
+            Description           = $Row[$inner].Description
+        }
     }
+        
 };
 
-#output to CSV
-$array | Export-Csv -NoTypeInformation -Delimiter "," -Force -Path $fullpath;
+# Output to CSV
+$Array | Export-Csv -NoTypeInformation -Delimiter "," -Force -Path $fullpath;
+
+# Forwarding Report
+$ForwardingReport = Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox | Select-Object UserPrincipalName, ForwardingSmtpAddress, DeliverToMailboxAndForward;
+
+# Develop file and path names based on date and time
+$FwdRTDateStr = $Date.ToString("yyyyMMdd-HHmm");
+$FwdRTfilename = $FwdRTDateStr + "_ForwardingReport.csv";
+$FwdRTfullpath = Join-Path -Path $BasePath -ChildPath $FwdRTfilename;
+$ForwardingReport | Export-Csv -NoTypeInformation -Force -Path $FwdRTfullpath;
+ 
+# present run-time
 $EndTime = (Get-Date);
 $TimeBetween = $EndTime - $StartTime;
-
-# present run-time
 Write-Host "Run time was"`n`t "Hours: " $TimeBetween.TotalHours `n`t "Minutes: " $TimeBetween.TotalMinutes `n`t "Seconds: " $TimeBetween.TotalSeconds;
